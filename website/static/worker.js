91ac42e477a9fe295223096f6e71bc2ab0890239
Frank Li <frankli1@fb.com>
2017-06-27 15:56:26 -0700

    Update Docusaurus structure

--- website/static/worker.js
+++ website/static/worker.js
@@ -16,7 +16,7 @@ self.require = function require(path) {
   return self[path.replace(/.+-/, "")];
 };
 
-importScripts("index.js");
+importScripts("lib/index.js");
 var prettier = index; // eslint-disable-line
 
 var parsersLoaded = {};
@@ -67,7 +67,7 @@ self.onmessage = function(message) {
 
 function lazyLoadParser(parser) {
   if (!parsersLoaded[parser]) {
-    importScripts("parser-" + parser + ".js");
+    importScripts("lib/parser-" + parser + ".js");
     parsersLoaded[parser] = true;
   }
 }
